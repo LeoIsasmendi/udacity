68dd56de874bfd8f8d70589e7c3e85a3285f672c
Leonardo Isasmendi <leoisas@gmail.com>
2016-04-19 11:20:30 -0300

    First commit of MyAppPortfolio

---
+++ MyAppPortfolio/app/src/main/java/com/example/android/myappportfolio/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.android.myappportfolio;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View v) {
+        Button btn = (Button) v.findViewById(v.getId());
+        this.showMessage(btn.getText() + "Btn pressed");
+    }
+
+    private void showMessage(String text) {
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+}
