35cdf2fcbc01175d55e0f758c67a337a2a6cae07
Leonardo Isasmendi <leoisas@gmail.com>
2016-05-23 20:12:29 -0300

    [project1] Added Error message

--- Project1/app/src/main/java/com/example/android/project1/MainActivity.java
+++ Project1/app/src/main/java/com/example/android/project1/MainActivity.java
@@ -1,18 +1,17 @@
 package com.example.android.project1;
 
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -129,8 +128,7 @@ public class MainActivity extends AppCompatActivity {
 
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        // TODO Auto-generated method stub
-
+                        Toast.makeText(getApplicationContext(), error.toString(), Toast.LENGTH_LONG).show();
                     }
                 });
         // Access the RequestQueue through your singleton class.
